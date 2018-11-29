9a4ee4bace9ea3de517ae21d7d3fb9837d6991a0
Bruno Conterato <brunoconterato@gmail.com>
2018-11-29 20:49:20 -0200

    Inclusão de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { CoreComponent } from './core/core.component';
+import { MainComponent } from './core/main/main.component';
+import { GameComponent } from './core/game/game.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'core',
+    pathMatch: 'full'
+  },
+  {
+    path: 'core',
+    component: CoreComponent,
+    pathMatch: 'prefix',
+    children: [
+      {
+        path: '',
+        redirectTo: 'main',
+        pathMatch: 'full'
+      },
+    ]
+  },
+  {
+    path: 'main',
+    component: MainComponent,
+  },
+  {
+    path: 'game',
+    component: GameComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
