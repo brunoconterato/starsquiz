c3250c496f0a1d7b7c7de198b6b2aa76a852f1f8
Bruno Conterato <brunoconterato@gmail.com>
2018-11-29 20:05:55 -0200

    Criada estrutura de componentes do game

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreComponent } from './core/core.component';
+import { MainComponent } from './core/main/main.component';
+import { GameComponent } from './core/game/game.component';
+import { CardComponent } from './core/game/card/card.component';
+import { TimerComponent } from './core/game/timer/timer.component';
+import { DetailsModalComponent } from './core/game/details-modal/details-modal.component';
+import { FinishModalComponent } from './core/game/finish-modal/finish-modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoreComponent,
+    MainComponent,
+    GameComponent,
+    CardComponent,
+    TimerComponent,
+    DetailsModalComponent,
+    FinishModalComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/core/game/finish-modal/finish-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-finish-modal',
+  templateUrl: './finish-modal.component.html',
+  styleUrls: ['./finish-modal.component.scss']
+})
+export class FinishModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
