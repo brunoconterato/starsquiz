cf2500cb1a87665059a1fbeeacba48bf703bb5e0
Bruno Conterato <brunoconterato@gmail.com>
2018-12-02 23:45:59 -0200

    Ao abrir modal de finalização, pontuação atualiza

--- src/app/core/game/finish-modal/finish-modal.component.ts
+++ src/app/core/game/finish-modal/finish-modal.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import * as _ from 'lodash';
 
 declare var $: any;
 
@@ -8,6 +9,11 @@ declare var $: any;
   styleUrls: ['./finish-modal.component.scss']
 })
 export class FinishModalComponent implements OnInit {
+  @Input() set boardPoints(pts) {
+    this._boardPoints = pts;
+  }
+  _boardPoints: any;
+  totalPoints: number = 0;
 
   constructor() { }
 
@@ -19,6 +25,22 @@ export class FinishModalComponent implements OnInit {
       show: true,
       backdrop: 'static',
       keyboard: false
-  })
+    });
+
+    this.finishGame();
+  }
+
+  finishGame() {
+    this.totalPoints = 0;
+    const boardArr = _.values(this._boardPoints);
+
+    for (let i = 0; i < boardArr.length; i++) {
+      if (boardArr[i].correct && !boardArr[i].gotInfo) {
+        this.totalPoints += 10;
+      } else if (boardArr[i].correct && boardArr[i].gotInfo) {
+        this.totalPoints += 5;
+      }
+    }
+
   }
 }
